e5315c6bbb2cd7fe97f57751f4504a07c57b88d9
tmccord26 <tmccord26@amherst.edu>
2025-12-13 18:28:39 -0500

    adding F1 score evaluation for log reg

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -2,12 +2,10 @@ import numpy as np
 import pandas as pd
 import pickle
 from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import f1_score, classification_report
 from src.data_preprocessing import load_data_sklearn
 
-def fit_logistic_regression():
-
-    # Retrieving the appropriate data formatting for logistic regression
-    X_train, X_val, X_test, y_train, y_val, y_test = load_data_sklearn()
+def fit_logistic_regression(X_train, y_train):
 
     model = LogisticRegression(
         max_iter = 2000,
@@ -23,6 +21,29 @@ def fit_logistic_regression():
 
     print(f"Logistic regression model successfully saved to {filepath}")
 
+    return model
+
+def evaluate(model, X_val, y_val):
+
+    y_pred = model.predict(X_val)
+    f1 = f1_score(y_val, y_pred, average="binary")
+
+    print("\nValidation Results")
+    print("------------------")
+    print(f"F1-score: {f1:.4f}\n")
+
+
+def main():
+
+    # Retrieving the appropriate data formatting for logistic regression
+    X_train, X_val, X_test, y_train, y_val, y_test = load_data_sklearn()
+
+    model = fit_logistic_regression(X_train, y_train)
+
+    evaluate(model, X_val, y_val)
+
+
+
 if __name__ == "__main__":
-    fit_logistic_regression()
+    main()
 
