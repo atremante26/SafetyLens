1685b6d1220d912d0cc2087cf3f141c33d3324cf
tmccord26 <tmccord26@amherst.edu>
2025-12-13 19:11:23 -0500

    updating logistic regression implementation with script

--- models/logistic_regression.py
+++ models/logistic_regression.py
@@ -5,7 +5,7 @@ from sklearn.linear_model import LogisticRegression
 from sklearn.metrics import f1_score, classification_report
 from src.data_preprocessing import load_data_sklearn
 
-def fit_logistic_regression(X_train, y_train):
+def fit_logistic_regression(X_train, y_train, PATH_MOD):
 
     model = LogisticRegression(
         max_iter = 2000,
@@ -13,37 +13,34 @@ def fit_logistic_regression(X_train, y_train):
         random_state=42
         )
     model.fit(X_train, y_train)
-
-    filepath = 'results/models/logistic_regression_model.pkl'
     # Saving model as .pkl file
-    with open(filepath, 'wb') as file:
+    with open(PATH_MOD, 'wb') as file:
         pickle.dump(model, file)
 
-    print(f"Logistic regression model successfully saved to {filepath}")
+    print(f"Logistic regression model successfully saved to {PATH_MOD}")
 
     return model
 
-def evaluate(model, X_val, y_val):
+def evaluate(model, X_val, y_val, PATH_PRED):
 
+    # Get predictions
+    logits = model.decision_function(X_val)
+    probs = model.predict_proba(X_val)[:,1]
     y_pred = model.predict(X_val)
+
+    # Save predictions
+    df_preds = pd.DataFrame({
+        "logit": logits,
+        "probability": probs,
+        "prediction": y_pred
+    })
+
+    df_preds.to_csv(PATH_PRED, index=False)
+
+    # Calculate f1 score
     f1 = f1_score(y_val, y_pred, average="binary")
 
     print("\nValidation Results")
     print("------------------")
     print(f"F1-score: {f1:.4f}\n")
-
-
-def main():
-
-    # Retrieving the appropriate data formatting for logistic regression
-    X_train, X_val, X_test, y_train, y_val, y_test = load_data_sklearn()
-
-    model = fit_logistic_regression(X_train, y_train)
-
-    evaluate(model, X_val, y_val)
-
-
-
-if __name__ == "__main__":
-    main()
 
---
+++ scripts/train_logistic_regression.py
@@ -0,0 +1,38 @@
+import argparse
+
+from models.logistic_regression import fit_logistic_regression
+from models.logistic_regression import evaluate
+
+from src.data_preprocessing import load_data_sklearn
+
+def parse_args():
+    p = argparse.ArgumentParser()
+    p.add_argument("--mod_out", required=True, help="Output model .pkl path")
+    p.add_argument("--preds_out", required=True, help="Output test predictions .csv path")
+    return p.parse_args()
+
+def main():
+
+    args = parse_args()
+
+    PATH_MOD = args.mod_out
+    PATH_PRED = args.preds_out
+
+    # Retrieving the appropriate data formatting for logistic regression
+    X_train, X_val, X_test, y_train, y_val, y_test = load_data_sklearn()
+
+    model = fit_logistic_regression(X_train, y_train, PATH_MOD)
+
+    evaluate(model, X_val, y_val, PATH_PRED)
+
+
+if __name__ == "__main__":
+    main()
+
+
+'''
+Example:
+python scripts/train_logistic_regression.py \
+  --mod_out results/models/logistic_regression_model.pkl \
+  --preds_out results/logistic_regression/test_preds.csv
+'''
