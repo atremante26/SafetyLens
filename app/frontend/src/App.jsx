28b243cdaa6fd9eceac18349de280df7e567a90b
atremante26 <atremante26@amherst.edu>
2025-12-23 17:59:37 -0500

    Updated scikit-learn version

--- app/frontend/src/App.jsx
+++ app/frontend/src/App.jsx
@@ -6,7 +6,7 @@ import About from './pages/About'
 
 function App() {
   return (
-    <Router>
+    <Router basename="/SafetyLens">  {/* ← ADD basename HERE */}
       <div className="app">
         <Toaster 
           position="top-right"
